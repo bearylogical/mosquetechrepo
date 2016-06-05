120245394d76dc4a304153c3f320812e1e695669
Syamil <syamil8@gmail.com>
2016-06-05 13:32:15 +0800

    implemented db and API

--- mosquetech.py
+++ mosquetech.py
@@ -1,8 +1,34 @@
 from flask import Flask
-
+from flask.ext.restless import APIManager
+from flask_sqlalchemy import SQLAlchemy
+import config
+import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
 
+
+class Device(db.Model):
+    __tablename__ = 'deviceList'
+
+    id = db.Column(db.Integer, primary_key=True)
+    deviceName = db.Column(db.String(64), index=True, unique=True)
+    transactions = db.relationship('Tabung', backref='device', lazy='dynamic')
+
+
+class Tabung(db.Model):
+    __tablename__ = 'transactionTable'
+
+    id = db.Column(db.Integer, primary_key=True)
+    transAmt = db.Column(db.Integer, index=True)
+    timestamp = db.Column(db.DateTime)
+    device_id = db.Column(db.Integer, db.ForeignKey('deviceList.id'))
+
+db.create_all()
+
+api_manager =  APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(Tabung,methods=['GET','POST','DELECT','PUT'])
 
 @app.route('/')
 def hello_world():
@@ -11,4 +37,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
-
