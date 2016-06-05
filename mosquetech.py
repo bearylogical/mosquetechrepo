21645f131858223d0199eae7bb5f8a561613439b
Syamil <syamil8@gmail.com>
2016-06-05 15:50:50 +0800

    added .gitignore and fixed sqlite date time error

--- mosquetech.py
+++ mosquetech.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.restless import APIManager
+from flask_restless import APIManager
 from flask_sqlalchemy import SQLAlchemy
 import config
 import datetime
@@ -22,18 +22,18 @@ class Tabung(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     transAmt = db.Column(db.Integer, index=True)
-    timestamp = db.Column(db.DateTime)
+    timestamp = db.Column(db.Integer)
     device_id = db.Column(db.Integer, db.ForeignKey('deviceList.id'))
 
-db.create_all()
-
 api_manager =  APIManager(app, flask_sqlalchemy_db=db)
-api_manager.create_api(Tabung,methods=['GET','POST','DELECT','PUT'])
+api_manager.create_api(Device,methods=['GET','POST','DELETE'])
+api_manager.create_api(Tabung,methods=['GET','POST','DELETE','PUT'])
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
+app.debug = True
 
 if __name__ == '__main__':
     app.run()
